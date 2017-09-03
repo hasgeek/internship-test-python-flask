685f40b71f66901cf257c33c77c9406f641d293f
kaushikthedeveloper <kaushikthedeveloper@gmail.com>
2017-09-03 23:00:22 +0530

    Completed the project with specified requirements implemented

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 globalScore = 0
-
 
 @app.route("/")
 def hello():
     global globalScore
     # Render the template with the globalScore data
-    return render_template("index.html")
+    return render_template("index.html", score=globalScore)
 
 
 # Define a new @app.route for '/count' with the method POST
@@ -16,6 +15,19 @@ def hello():
 # if the value is 'increment', increase globalScore by 1
 # else if the value is 'decrement' decrease globalScore by 1
 # and finally issue a redirect back to '/' so the new score is shown
+
+@app.route("/count", methods=['POST'])
+def countOperation():
+    global globalScore
+
+    #Increment/Decrement count value
+    if( request.form['count'] == 'increment' ):
+        globalScore+=1
+    else:
+        globalScore-=1
+
+    #Redirect to '/'
+    return redirect(url_for('hello'))
 
 if __name__ == "__main__":
     app.run()
